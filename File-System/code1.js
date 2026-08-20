c54160b958e67c1a6fdcf4a67a33d5503c1b4393
Aakash <aakash76k747@gmail.com>
2026-08-20 12:09:52 +0530

    Update read and write in File-system

---
+++ File-System/code1.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+// read file
+fs.readFile("ex.txt", "utf-8" , (err, data) =>{
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.log("data : ", data);
+});
+
+// write file
+
+fs.writeFile("ex.txt", "tell" , (err)=>{
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.log("File created successfully ");
+});
